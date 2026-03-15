062187776cd82413290fb2bcd86aae2f594271a6
Danihmn <daniel.bezerra.mult@gmail.com>
2026-03-15 20:10:35 -0300

    Adiciona passos a serem executados no Script do GitHub Actions

--- AppointmentScheduler/AppointmentScheduler/Configurations/ScalarConfiguration.cs
+++ AppointmentScheduler/AppointmentScheduler/Configurations/ScalarConfiguration.cs
@@ -2,14 +2,12 @@
 
 public static class ScalarConfiguration
 {
-    private static readonly string AppName = "API APPOINTMENT SCHEDULER";
-
     public static WebApplication UseScalarDocumentation (this WebApplication app)
     {
         app.MapScalarApiReference("/scalar", options =>
         {
             options
-                .WithTitle(AppName)
+                .WithTitle("API APPOINTMENT SCHEDULER")
                 .WithOpenApiRoutePattern("/openapi/v1.json");
         });
 
