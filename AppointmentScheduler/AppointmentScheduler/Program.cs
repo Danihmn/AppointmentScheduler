8179095f92454555ef4d27513246e74bc4daea1a
Danihmn <daniel.bezerra.mult@gmail.com>
2026-04-07 23:26:37 -0300

    Configura API Gateway com ngrok no docker-compose para testes locais

--- AppointmentScheduler/AppointmentScheduler/Program.cs
+++ AppointmentScheduler/AppointmentScheduler/Program.cs
@@ -29,8 +29,17 @@ public class Program
 
         builder.Services.AddOpenApi();
 
+        builder.Services.Configure<ForwardedHeadersOptions>(options =>
+        {
+            options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+            options.KnownNetworks.Clear();
+            options.KnownProxies.Clear();
+        });
+
         var app = builder.Build();
         var isDevelopment = app.Environment.IsDevelopment();
+
+        app.UseForwardedHeaders();
 
         await app.SeedDatabaseAsync();
 
